c598a15f508979fdf8676908408dde79cf5b152c
Maliy Igor <imaliy@maximagroup.com>
2014-01-25 17:49:33 +0200

    Remove define

--- src/jquery.autocomplete.js
+++ src/jquery.autocomplete.js
@@ -13,13 +13,7 @@
 // Expose plugin as an AMD module if AMD loader is present:
 (function (factory) {
     'use strict';
-    if (typeof define === 'function' && define.amd) {
-        // AMD. Register as an anonymous module.
-        define(['jquery'], factory);
-    } else {
-        // Browser globals
-        factory(jQuery);
-    }
+    factory(jQuery);
 }(function ($) {
     'use strict';
 
